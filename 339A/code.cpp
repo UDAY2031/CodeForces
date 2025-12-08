5c074e110be8ad80a0bfa01510c617512c4547e4
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2025-12-08 22:46:45 +0530

    Create code.cpp

---
+++ 339A/code.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin >> s;
+    
+    vector<char> res;
+    
+    for(int i = 0; i < s.size(); i++){
+        if(isalnum(s[i])){
+            res.push_back(s[i]);
+        }
+    }
+    sort(res.begin(), res.end());
+    for(int i = 0; i < res.size(); i++){
+        cout << res[i];
+        if(i < res.size()-1){
+            cout << "+";
+        }
+    }
+    return 0;
+}
